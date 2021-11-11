06a983225ab5cde3ac207d5379969b82556b9573
Raúl <raul.alonso.merino@gmail.com>
2021-11-11 23:39:16 +0100

    first iteration

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 // Iteration 1: Names and Input
-// 
+let hacker1 = "Raúl";
+console.log(`The driver's name is ${hacker1}.`);
+let hacker2 = "Elisa";
+console.log(`The navigator's name is ${hacker2}`);
 
 // Iteration 2: Conditionals
 
